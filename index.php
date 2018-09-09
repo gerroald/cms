a5419d7584ffd81b91587ceaace6fec9f495dc16
Gerroald H Barron <gerroald@Gerroalds-MacBook-Pro.local>
2018-09-08 22:38:35 -0500

    Added post image variable

--- index.php
+++ index.php
@@ -16,15 +16,15 @@
                 <?php
                 $query = "SELECT * FROM posts";
                 $select_all_posts_query = mysqli_query($connection, $query);
-                    
+
                 while($row =  mysqli_fetch_assoc($select_all_posts_query)) {
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];                    
+                    $post_content = $row['post_content'];
                     ?>
-                    
+
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -39,7 +39,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
@@ -47,7 +47,7 @@
 
                 <?php } ?>
             </div>
-            
+
 
             <!-- Blog Sidebar Widgets Column -->
 <?php include "includes/sidebar.php" ?>
