2fa58e3c84576ab154bd6caad4671b45051e4a1f
Gerroald Barron <gerroald@Gerroalds-MacBook-Pro.local>
2018-09-12 17:13:57 -0500

    Added create category functionality

--- admin/categories.php
+++ admin/categories.php
@@ -1,8 +1,8 @@
-<?php include "includes/header.php" ?>
+<?php include "includes/admin_header.php" ?>
 
     <div id="wrapper">
         <!-- Navigation -->
-<?php include "includes/navigation.php" ?>
+<?php include "includes/admin_navigation.php" ?>
 
         <div id="page-wrapper">
 
@@ -16,7 +16,24 @@
                             <small>Author</small>
                         </h1>
                         <div class="col-xs-6">
-                        <form action="">
+                        <?php
+                        if(isset($_POST['submit'])) {
+                        
+                            $cat_title = $_POST['cat_title'];
+                        if($cat_title == "" || empty($cat_title)) {
+                            echo "This field should not be empty.";
+                        } else {
+                            $query = "INSERT INTO categories(cat_title)";
+                            $query .= "VALUE('{$cat_title}') ";
+                            
+                            $create_category_query = mysqli_query($connection,$query);
+                            if(!$create_category_query) {
+                                die('QUERY FAILED' . mysqli_error($connection));
+                            }
+                        }
+                        }
+                        ?>
+                        <form action="" method="post">
                             <div class="form-group">
                                <label for="cat_title">Add Category</label>
                                 <input class="form-control" type="text" name="cat_title">
@@ -25,6 +42,35 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+                        
+                        
+                        </div> <!-- /.col-xs-6 - Category form-->
+                        <div class="col-xs-6">
+                            <?php
+                            $query = "SELECT * FROM categories";
+                            $select_categories = mysqli_query($connection, $query);
+                            ?>
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    while($row =  mysqli_fetch_assoc($select_categories)) {
+                                    $cat_id = $row['cat_id'];
+                                    $cat_title = $row['cat_title'];
+                                        
+                                    echo "<tr>";
+                                    echo "<td>{$cat_id}</td>";
+                                    echo "<td>{$cat_title}</td>";
+                                    echo "</tr>";
+                                    }
+                                    ?> 
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -33,4 +79,4 @@
             </div>
             <!-- /.container-fluid -->
 
-<?php include "includes/footer.php" ?>
+<?php include "includes/admin_footer.php" ?>
