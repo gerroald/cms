c00242435084ff481c295a9cf55983deabc52512
Gerroald H Barron <gerroald@Gerroalds-MacBook-Pro.local>
2018-09-18 20:30:21 -0500

    Conditionally include admin pages

--- admin/posts.php
+++ admin/posts.php
@@ -15,64 +15,31 @@
 							Welcome to admin.
 							<small>Author</small>
 						</h1>
-
-						<table class="table table-bordered table-hover">
-							<thead>
-								<tr>
-									<th>Id</th>
-									<th>Author</th>
-									<th>Title</th>
-									<th>Category</th>
-									<th>Status</th>
-									<th>Image</th>
-									<th>Tags</th>
-									<th>Comments</th>
-									<th>Date</th>
-								</tr>
-							</thead>
-							<tbody>
 <?php
-$query = "SELECT * FROM posts";
-$select_posts = mysqli_query($connection, $query);
+if(isset($_GET['source'])) {
 
-while($row =  mysqli_fetch_assoc($select_posts)) {
-    $post_id = $row['post_id'];
-    $post_author = $row['post_author'];
-    $post_title = $row['post_title'];
-    $post_category_id = $row['post_category_id'];
-    $post_status = $row['post_status'];
-    $post_image = $row['post_image'];
-    $post_tags = $row['post_tags'];
-    $post_comment_count = $row['post_comment_count'];
-    $post_date = $row['post_date'];
-    
-    echo "<tr>";
-    echo "<td>{$post_id}</td>";
-    echo "<td>{$post_author}</td>";
-    echo "<td>{$post_title}</td>";
-    echo "<td>{$post_category_id}</td>";
-    echo "<td>{$post_status}</td>";
-    echo "<td><img width='100' src='../images/$post_image' alt='images'></td>";
-    echo "<td>{$post_tags}</td>";
-    echo "<td>{$post_comment_count}</td>";
-    echo "<td>{$post_date}</td>";
-    echo "</tr>";
-    
-    
+	$source = $_GET['source'];
+} else {
+	$source = "";
+}
+switch($source = '34') {
+	case '34':
+		echo 'Nice, 34!';
+		break;
+
+	case '100':
+		echo 'Nice, 100!';
+		break;
+
+	case '200':
+		echo 'Nice, 200!';
+		break;
+
+	default:
+		include "includes/view_all_posts.php";
+		break;
 }
 ?>
-								<td>Id</td>
-								<td>Gerroald Barron</td>
-								<td>Bootstrap Framework</td>
-								<td>Bootstrap</td>
-								<td>Status</td>
-								<td>Image</td>
-								<td>Tags</td>
-								<td>Comments</td>
-								<td>Date</td>
-
-							</tbody>
-						</table>
 					</div>
 				</div>
 				<!-- /.row -->
