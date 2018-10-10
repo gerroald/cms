1c3d462e4af0b577314116481977fea58b598a89
Gerroald Barron <gerroald@Gerroalds-MacBook-Pro.local>
2018-10-10 17:05:14 -0500

    Added ability to delete posts

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -38,10 +38,11 @@ while($row =  mysqli_fetch_assoc($select_posts)) {
 	echo "<td>{$post_tags}</td>";
 	echo "<td>{$post_comment_count}</td>";
 	echo "<td>{$post_date}</td>";
+	echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
 	echo "</tr>";
 }
 ?>
-								<td>Id</td>
+								<td>20</td>
 								<td>Gerroald Barron</td>
 								<td>Bootstrap Framework</td>
 								<td>Bootstrap</td>
@@ -53,3 +54,13 @@ while($row =  mysqli_fetch_assoc($select_posts)) {
 
 							</tbody>
 						</table>
+						
+<?php
+if(isset($_GET['delete'])) {
+
+    $the_post_id = $_GET['delete'];
+    
+    $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
+    $delete_query = mysqli_query($connection, $query);
+}
+?>
